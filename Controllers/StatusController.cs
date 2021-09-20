6585894181d4d353ac6cbee42b97ec0035707dc0
Tushar Harsora <tusharharsora95@gmail.com>
2021-09-20 11:56:14 +0530

    Added initial project with docker container
    Currently it has example WeatherForcast api
    and other endpoint for checking server is running

---
+++ Controllers/StatusController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecureChatServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        public StatusController(ILogger<StatusController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("Server status checked");
+            return "Server is Up and Running!";
+        }
+    }
+}
