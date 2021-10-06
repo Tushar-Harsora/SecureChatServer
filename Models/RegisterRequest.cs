7ddd86314f271c7f0d1ed74280c372257bb3755f
Tushar Harsora <tusharharsora95@gmail.com>
2021-10-06 13:52:57 +0530

    Made optional members nullable

--- Models/RegisterRequest.cs
+++ Models/RegisterRequest.cs
@@ -3,8 +3,8 @@
     public class RegisterEmailRequest
     {
         public string email { get; set; }
-        public string phone_number { get; set; }
-        public string username { get; set; }
+        public string? phone_number { get; set; }
+        public string? username { get; set; }
         public string public_key { get; set; }
 
     }
