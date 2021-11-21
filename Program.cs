cfd4b41f40e55ed08b223bf1e9f447cbda94ff4e
Bhavin-Kareliya <bhavin.kareliya2017@gmail.com>
2021-11-21 11:32:53 +0530

    Now server listens on 0.0.0.0

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using SecureChatServer.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:5000;https://0.0.0.0:5001;");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
