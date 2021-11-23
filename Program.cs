447e288cc74a130f86d33683ff37d5e4a013d578
Bhavin-Kareliya <bhavin.kareliya2017@gmail.com>
2021-11-23 21:34:13 +0530

    update project configuration for release build

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using SecureChatServer.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls("http://0.0.0.0:5000;https://0.0.0.0:5001;");
+builder.WebHost.UseUrls("http://*:2000;https://*:2001");
 
 // Add services to the container.
 
